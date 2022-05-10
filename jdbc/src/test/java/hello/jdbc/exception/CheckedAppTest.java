588d3fd2a3b442294eadaec2af9d16e92e7150bd
Pipi-Olo <Copy.H13@gmail.com>
2022-05-10 20:46:47 +0900

    Add - Checked & Uncheked Exception Test

---
+++ jdbc/src/test/java/hello/jdbc/exception/CheckedAppTest.java
@@ -0,0 +1,49 @@
+package hello.jdbc.exception;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CheckedAppTest {
+
+    @Test
+    void checked() {
+        Controller controller = new Controller();
+        assertThatThrownBy(() -> controller.request())
+                .isInstanceOf(Exception.class);
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() throws SQLException, ConnectException {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        NetworkClient networkClient = new NetworkClient();
+        Repository repository = new Repository();
+
+        public void logic() throws ConnectException, SQLException {
+            networkClient.call();
+            repository.call();
+        }
+    }
+
+    static class NetworkClient {
+        public void call() throws ConnectException {
+            throw new ConnectException();
+        }
+    }
+
+    static class Repository {
+        public void call() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+}
